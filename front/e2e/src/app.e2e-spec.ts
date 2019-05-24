877d47bde6794f4637f47210ed191e6434f8e893
Silva Neto <silva.neto.sistemas@gmail.com>
2019-05-24 01:11:03 -0300

    Adicionando testes automatizados de todas as funcionalidades do sistema.

--- front/e2e/src/app.e2e-spec.ts
+++ front/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,56 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  const waitTime = 5000;
+  const waitTimeToViewValue = 2000;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to handle-product!');
+  it('Cadastrar um produto', () => {
+    page.navigateToPageProduct();
+    page.getDataToField();
+    page.clickSaveButton();
+    browser.wait(browser.getCurrentUrl(), waitTime).then(result => {
+      browser.sleep(waitTimeToViewValue);
+      expect(result).toEqual('http://localhost:4200/product-list');
+    });
   });
+
+  it('Direcionar para a tela de listagem de produtos, e verificar se o item foi adicionado', () => {
+    let last = element.all(by.id('name')).last();
+    browser.wait(browser.getCurrentUrl(), waitTime).then(() => {
+      browser.sleep(waitTimeToViewValue);
+      expect(last.getText()).toBe('Asus VivoBook X510UR-BQ292T Intel Core i7-7500U 2.7 GHz 8192 MB 1024 GB');
+    });
+  });
+
+  it('Clickar no botão de editar e editar os dados do valor do notebook', () => {
+    let last;
+    page.clickEditButton();
+    browser.wait(browser.getCurrentUrl(), waitTime).then(() => {
+      browser.sleep(waitTimeToViewValue);
+      page.clearValueData();
+      page.changeValueData();
+      browser.sleep(waitTimeToViewValue);
+      page.clickSaveButton();
+      browser.sleep(waitTimeToViewValue);
+    }).then(() => {
+      last = element.all(by.id('value')).last();
+    }).then(() => {
+      browser.sleep(waitTimeToViewValue);
+      expect(last.getText()).toEqual('3.555');
+    });
+  });
+
+  it('Deletar o dado inserido de forma automatizada', () => {
+    page.clickDeleteButton();
+    let last = element.all(by.id('name')).last();
+    browser.sleep(waitTime);
+    expect(last.getText()).not.toBe('Asus VivoBook X510UR-BQ292T Intel Core i7-7500U 2.7 GHz 8192 MB 1024 GB');
+  });
+
 });
--- front/e2e/src/app.po.ts
+++ front/e2e/src/app.po.ts
@@ -1,11 +1,63 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/product');
+  navigateToPageProduct() {
+    return browser.get('http://localhost:4200/product');
   }
 
-  getTitleText() {
-    return element(by.css('[id="name"]')).sendKeys('Glub');
+  navigateToPageListProduct() {
+    return browser.get('http://localhost:4200/product-list');
+  }
+
+  /**
+   * @description Esse método adiciona os valores de nome e valor do produto
+   */
+  getDataToField() {
+    element(by.id('name')).clear();
+    element(by.id('value')).clear();
+    element(by.id('name')).sendKeys('Asus VivoBook X510UR-BQ292T Intel Core i7-7500U 2.7 GHz 8192 MB 1024 GB');
+    element(by.id('value')).sendKeys(5.999);
+  }
+
+  /**
+   * @description Método responsável por acionar o botão de cadastrar
+   */
+  clickSaveButton() {
+    element(by.id('btn-cadastrar')).click();
+  }
+
+  /**
+   * @description Esse método é responsável por acionar o click para direcionar para a tela de listar produtos
+   */
+  clickListButton() {
+    element(by.id('btn-listar')).click();
+  }
+
+  /**
+   * @description Esse método é responsável por acionar o botão do último elemento para edita-lo
+   */
+  clickEditButton() {
+    element.all(by.id('btn-editar')).last().click();
+  }
+
+  /**
+   * @description Esse método é responsável por acionar o botão do último elemento para edita-lo
+   */
+  clickDeleteButton() {
+    element.all(by.id('btn-deletar')).last().click();
+  }
+
+  /**
+   * @description Esse método adiciona o valor do último produto
+   */
+  changeValueData() {
+    element.all(by.id('value')).last().sendKeys(3.555);
+  }
+
+  /**
+   * @description Esse método adiciona o valor do último produto
+   */
+  clearValueData() {
+    element.all(by.id('value')).last().clear();
   }
 }
